f24fa654fb0ed5a20c1337f389cba55840013a86
Roberto Picanço <70612028+betopicanco@users.noreply.github.com>
2022-10-22 09:18:12 -0300

    adjust user

--- src/main/java/br/com/sharetips/entities/User.java
+++ src/main/java/br/com/sharetips/entities/User.java
@@ -5,8 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.*;
